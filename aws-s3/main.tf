b62d0d5c693e702ca9d4195689093b12eab7e4a3
Prachiti <wprachiti@gmail.com>
2026-09-12 19:49:54 +0530

    created S3 bucket

---
+++ aws-s3/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+    bucket = "my_terraform_s3_bucket"
+  
+}
