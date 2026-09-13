1bd36474c383b5ffa471ca9b2bc664a0a0f02f95
Prachiti <wprachiti@gmail.com>
2026-09-13 13:25:27 +0530

    added code for nginix server

--- aws-vpc-ec2-ngnix/main.tf
+++ aws-vpc-ec2-ngnix/main.tf
@@ -2,15 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.9.1"
+      version = "5.54.1"
     }
   }
-}
-
-provider "aws" {
-  region = "ap-south-1"
 }
