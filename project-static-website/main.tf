0ea1363be89a7d6bc032d8b9bb5dfc8df0ce8b38
Prachiti <wprachiti@gmail.com>
2026-09-13 10:53:33 +0530

    added publick access policy

--- project-static-website/main.tf
+++ project-static-website/main.tf
@@ -25,6 +25,16 @@ resource "aws_s3_bucket" "mywebapp_bucket" {
   
 }
 
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mywebapp_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.mywebapp_bucket.id
   key    = "index.html"
