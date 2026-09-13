dd7c63661531a01e5ab9eb169bc383b7447d4e24
Prachiti <wprachiti@gmail.com>
2026-09-13 13:27:52 +0530

    changed ami

--- aws-vpc-ec2-ngnix/ec2.tf
+++ aws-vpc-ec2-ngnix/ec2.tf
@@ -1,8 +1,8 @@
 
 # EC2 instance For Nginx setup
 resource "aws_instance" "nginxserver" {
-  ami                         = "ami-0c0e147c706360bd7"
-  instance_type               = "t3.nano"
+  ami                         = "ami-08188a5a4dfdbd573"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public-subnet.id
   vpc_security_group_ids      = [aws_security_group.nginx-sg.id]
   associate_public_ip_address = true
