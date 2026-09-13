b10e254299c2f821ae4ba94404f40fd191623462
Prachiti <wprachiti@gmail.com>
2026-09-13 10:39:47 +0530

    added new static website code

---
+++ project-static-website/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.9.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "random_id" "my_random_id" {
+  byte_length = 8
+  
+}
+
+resource "aws_s3_bucket" "mywebapp_bucket" {
+    bucket = "mywebapp-bucket-${random_id.my_random_id.hex}"
+  
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.mywebapp_bucket.id
+  key    = "index.html"
+  source = "./index.html"
+  
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.mywebapp_bucket.id
+  key    = "styles.css"
+  source = "./styles.css"
+  
+}
+
+output "name" {
+  value = random_id.my_random_id.hex
+}
