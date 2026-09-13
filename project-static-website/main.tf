625b6310f604f2ad7de3736f562d482d534d81b4
Prachiti <wprachiti@gmail.com>
2026-09-13 11:01:39 +0530

    added bucket policy

--- project-static-website/main.tf
+++ project-static-website/main.tf
@@ -35,6 +35,32 @@ resource "aws_s3_bucket_public_access_block" "example" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "mywebapp" {
+  bucket = aws_s3_bucket.mywebapp_bucket.id
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Sid       = "PublicReadGetObject",
+          Effect    = "Allow",
+          Principal = "*",
+          Action    = "s3:GetObject",
+          Resource  = "${aws_s3_bucket.mywebapp_bucket.arn}/*"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_s3_bucket_website_configuration" "mywebapp" {
+  bucket = aws_s3_bucket.mywebapp_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.mywebapp_bucket.id
   key    = "index.html"
